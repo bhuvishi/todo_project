71f48e3840e07bb3c8c2e8f497934bec9f015a1c
Sujeet Bansal <sujeetba@amazon.com>
2023-09-24 08:39:52 -0700

    first commit

---
+++ todo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home_page(request):
+    return render(request, 'todo/home.html')
+
+def about_page(request):
+    return render(request, 'todo/about.html')
